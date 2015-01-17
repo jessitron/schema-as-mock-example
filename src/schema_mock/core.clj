7f2bd08fd3956540bd6189531ee2ec37def9cdd8
Jessica Kerr <jessitron@gmail.com>
2015-01-17 14:20:26 -0600

    Schemas pass, regular failure

--- src/schema_mock/core.clj
+++ src/schema_mock/core.clj
@@ -19,7 +19,7 @@
   (let [end-date (time/minus (time/now) (time/hours 1))
         orders (fetch-orders start-date end-date)]
     (for [o orders]
-      (let [clicks (fetch-clicks (:when o) (conversion-period-for o) (:who o))]
+      (let [clicks (fetch-clicks (conversion-period-for o) (:when o) (:who o))]
         (notice-conversions o clicks)))))
 
 (defn- notice-conversions [orders clicks]
